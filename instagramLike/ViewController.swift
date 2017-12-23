2d112e8dc68e324c04b40d3da7d691fada4970a9
sasakazu <dropout.college2014@gmail.com>
2017-12-23 12:44:28 +0900

    Initial Commit

---
+++ instagramLike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagramLike
+//
+//  Created by 笹倉 一也 on 2017/12/23.
+//  Copyright © 2017年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
